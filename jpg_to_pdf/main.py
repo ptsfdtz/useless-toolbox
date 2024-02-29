a769f651d28e3aa81b6112cfb72dcc5d2c160034
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-02-29 19:22:30 +0800

    feat:add speedtest and jpg to pdf

---
+++ jpg_to_pdf/main.py
@@ -0,0 +1,41 @@
+from PIL import Image
+from reportlab.pdfgen import canvas
+import os
+
+def images_to_pdf(image_folder, output_pdf):
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png") or img.endswith(".jpg") or img.endswith(".JPG")]
+
+    if not images:
+        print("没有找到图片文件！")
+        return
+
+    images.sort()
+
+    pdf_path = os.path.join(output_pdf, "output.pdf")
+    c = canvas.Canvas(pdf_path)
+
+    for image in images:
+        img_path = os.path.join(image_folder, image)
+        img = Image.open(img_path)
+        width, height = img.size
+        c.setPageSize((width, height))
+        c.drawInlineImage(img_path, 0, 0, width, height)
+
+        if image != images[-1]:
+            c.showPage()
+
+    c.save()
+    print(f"PDF已生成:{pdf_path}")
+
+if __name__ == "__main__":
+    # 指定包含图片的文件夹路径
+    image_folder_path = "002/image"
+
+    # 指定输出PDF的文件夹路径
+    output_pdf_path = "002/output"
+
+    # 创建输出文件夹（如果不存在）
+    os.makedirs(output_pdf_path, exist_ok=True)
+
+    # 执行转换
+    images_to_pdf(image_folder_path, output_pdf_path)
---
+++ speed_test/main.py
@@ -0,0 +1,47 @@
+import speedtest
+import tkinter as tk
+from tkinter import Label, Button
+
+def run_speed_test():
+    print("准备测试ing...")
+
+    # 创建实例对象
+    test = speedtest.Speedtest()
+    # 获取可用于测试的服务器列表
+    test.get_servers()
+    # 筛选出最佳服务器
+    best = test.get_best_server()
+
+    print("正在测试ing...")
+
+    # 下载速度
+    download_speed = int(test.download() / 1024 / 1024)
+    upload_speed = int(test.upload() / 1024 / 1024)
+
+    # 更新界面显示
+    download_label.config(text="Download Speed:" + str(download_speed) + " Mbits")
+    upload_label.config(text="Upload Speed:" + str(upload_speed) + " Mbits")
+
+# 创建主窗口
+window = tk.Tk()
+window.title("Speed Test GUI")
+
+# 设置窗口大小
+window.geometry("400x200")  # You can change the width and height as needed
+
+# 添加标签和按钮
+status_label = Label(window, text="Click Start")
+download_label = Label(window, text="")
+upload_label = Label(window, text="")
+test_button = Button(window, text="运行速度测试", command=run_speed_test)
+
+# 布局管理
+status_label.pack(pady=10)
+test_button.pack(pady=10)
+download_label.pack()
+upload_label.pack()
+
+# No mainloop() here
+
+# The window will stay open until explicitly closed
+window.mainloop()
