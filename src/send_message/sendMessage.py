52c371e0bd20f9e644877fc12312d81db2451f0d
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-03-07 22:52:13 +0800

    fix:fix folder way

--- src/send_message/sendMessage.py
+++ src/send_message/sendMessage.py
@@ -45,7 +45,7 @@ def send_email(email_address, email_password, filename, recipient_name, recipien
     smtp.quit()
 
 def read_message_data():
-    file_path = 'src/send_message/index.xlsx'
+    file_path = 'index.xlsx'
     data = pd.read_excel(file_path)
     result_dict = data.set_index('姓名')['邮箱'].to_dict()
     return result_dict
